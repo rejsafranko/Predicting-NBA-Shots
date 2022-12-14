fc32e7c5a62d103a237242580d037642bc17634e
rjsfrnk <rej.safranko@gmail.com>
2022-12-14 14:06:03 +0100

    nb and rf training script, analysis notebook update

---
+++ src/train_randomforest_shotlogs_clf.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from argparse import ArgumentParser
+from joblib import dump
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("filename")
+    parser.add_argument("test_size", type=float)
+    return parser.parse_args()
+
+
+def load_data(filename, test_size):
+    df = pd.read_csv(filename, index_col=0)
+    labels = df["SHOT_RESULT"]
+
+    # Remove categoric data and labels.
+    features = df.drop(
+        ["CLOSEST_DEFENDER_PLAYER_ID", "player_id", "SHOT_RESULT"], axis=1
+    )
+
+    # Join features and labels.
+    df = pd.concat([features, labels], axis=1)
+
+    # Create dataset split.
+    dataset = dict()
+    train, test = train_test_split(df, test_size=test_size)
+    dataset["train"] = {
+        "features": train.loc[:, train.columns != "SHOT_RESULT"],
+        "labels": train["SHOT_RESULT"],
+    }
+    dataset["test"] = {
+        "features": test.loc[:, test.columns != "SHOT_RESULT"],
+        "labels": test["SHOT_RESULT"],
+    }
+
+    return dataset
+
+
+def main(args):
+    dataset = load_data(args.filename, args.test_size)
+    model = RandomForestClassifier()
+
+    # Train the model.
+    model.fit(dataset["train"]["features"], dataset["train"]["labels"])
+
+    # Save the model.
+    dump(model, "./models/randomforest.joblib")
+
+    # Save test data.
+    dump(dataset["test"], "./data/test data/randomforest_test_data.joblib")
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
